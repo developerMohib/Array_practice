e7b003410cfa24f36967b681b854962b657bcb79
Mohib <mohibullahmohim2020@gmail.com>
2024-01-31 01:27:36 +0600

    The task done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console log the final array
 
 */
 
-let fruits = [ 'jam', 'amm', 'peara', 'komola', 'licu' ];
+let fruits = [ ' jam', ' amm', ' peara', ' komola', ' licu' ];
 
 document.write(fruits);
 
@@ -15,3 +15,26 @@ document.write('<br> the lenth of array .' + fruits.length );
 
 let theThirdNum = fruits[3];
 document.write( '<br> the third num of array .' + theThirdNum);
+
+let lastOneOut = fruits.pop();
+document.write( '<br> remove last one from array .' + lastOneOut);
+
+document.write('<br> ' +fruits);
+
+// add two element
+
+let add_last_one = fruits.push(' jamboora' , ' koola');
+
+document.write('<br> ' + fruits);
+
+// Change the 3rd Number Value
+
+let serial = fruits.indexOf(' koola');
+document.write('<br> koolar serial number = ' + serial);
+
+fruits[2] = ' Pepe';
+
+let add_pepe = fruits;
+
+document.write('<br> pepe added = ' + add_pepe);
+
