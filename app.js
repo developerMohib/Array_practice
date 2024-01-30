37c667eba96038d4b6a7431864613f14bf9b3b45
Mohib <mohibullahmohim2020@gmail.com>
2024-01-30 23:43:05 +0600

    The First Commit to add repo

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+
+*/
+
+let fruits = [ 'amm, jamm, katal, peara, boroi'];
+
+document.write(fruits);
